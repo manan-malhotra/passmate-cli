552a9d5329ef226f3e7b48d0dd640f2d891c99e8
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2024-06-28 18:09:59 +0530

    register feature added

---
+++ helpers/register.js
@@ -0,0 +1,27 @@
+const readlineSync = require("readline-sync");
+const axios = require("axios").default;
+
+async function register() {
+  const username = readlineSync.question("Enter your username: ");
+  const password = readlineSync.question("Enter your password: ", {
+    hideEchoBack: true,
+  });
+  try {
+    const response = await axios.post(
+      "http://localhost:3000/api/user/register",
+      {
+        username,
+        password,
+      }
+    );
+    console.log(username + " registered successfully!");
+    console.log("Please login again to continue");
+  } catch (error) {
+    console.error(
+      "Register failed:",
+      error.response ? error.response.data : error.message
+    );
+  }
+}
+
+module.exports = { register };
