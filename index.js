e98246ae234b37cb815bbdaa32957409516ea4ee
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2024-06-28 18:13:31 +0530

    Help added for unknown commands

--- index.js
+++ index.js
@@ -8,9 +8,8 @@ const { whoami } = require("./helpers/whoami");
 console.log("Welcome to the CLI Login Tool!");
 const args = process.argv.slice(2);
 if (args.length === 0) {
-  console.log(
-    "Please provide a command: register, login, logout, whoami, addpass, viewpass"
-  );
+  console.log("Please provide a valid command");
+  console.log("Use passmate -help for more information");
   process.exit(1);
 }
 
@@ -34,8 +33,19 @@ switch (command) {
   case "viewpass":
     // viewpass()
     break;
+  case "-help":
+  case "help":
+  case "-h":
+    console.log("Available commands:");
+    console.log("  register");
+    console.log("  sudo login");
+    console.log("  whoami");
+    console.log("  addpass");
+    console.log("  viewpass");
+    console.log("  logout");
+    break;
   default:
     console.log(`Unknown command: ${command}`);
-    console.log('Use "register", "login", "logout", or "check-session".');
+    console.log("Use passmate -help for more information");
     process.exit(1);
 }
